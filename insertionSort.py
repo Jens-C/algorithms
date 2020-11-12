0aa1dd33186e3ab7a844ac83465fe6aa896b477e
Jens-C <Jens-Desktop@users.noreply.github.com>
2020-11-12 11:04:48 +0100

    created insertion sort

--- insertionSort.py
+++ insertionSort.py
@@ -1,6 +1,18 @@
+def insertionSort(arrey):
+    for i in range(1, len(arrey)):
+        big = 0
+        small = 0
+        index = i - 1
+        
+        while(index >= 0 and arrey[i] <= arrey[index]):
+            big = arrey[i]
+            small = arrey[index]
+            arrey[i] = small
+            arrey[index] = big
+            index -= 1
+            i -= 1
+            print
+    return arrey
+
 values = [-10, 8400, 1.77, 10, 69, 42, 2.33333]
-
-compare = 0
-compare2 = 1
-
-
+print(insertionSort(values))
